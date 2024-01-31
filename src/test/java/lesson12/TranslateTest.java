2167db95065df7d2f093253de82bf4bdf4c17b77
denis.p <denis.popov@gen.tech>
2024-01-31 22:06:38 +0200

    hw-lesson-10-TranslateTest

---
+++ src/test/java/lesson12/TranslateTest.java
@@ -0,0 +1,65 @@
+package lesson12;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class TranslateTest {
+
+    @BeforeClass
+    public void setUp() {
+
+        Configuration.timeout = 5000;
+
+        open("https://translate.google.com/?sl=uk&tl=en&op=translate");
+    }
+
+    @Test(dataProvider = "languages")
+    public void testGoogleTranslate(String language, String translation) {
+
+
+
+        $x("//*[contains(@class,'er8xn')]").should(appear).setValue("Я круто вивчу ТестНГ");
+        $("#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb.EjH7wc > div.aCQag > c-wiz > div.zXU7Rb > c-wiz > div:nth-child(5) > button > div.VfPpkd-Bz112c-RLmnJb").should(appear).click();
+
+        $$x("//*[contains(@class,'Llmcnf')]")
+                .filter(Condition.text(language))
+                .get(0).click();
+
+
+        $x("//*[contains(@class,'HwtZe')]").shouldHave(text(translation));
+
+
+    }
+
+    @DataProvider(name = "languages")
+    public Object[][] languages() {
+        return new Object[][]{
+                {"English", "I will learn TestNG cool"},
+                {"Bulgarian", "Ще науча TestNG яко"},
+                {"Chinese (Traditional)", "我將研究測試"},
+                {"Czech", "Naučím se TestNG v pohodě"},
+                {"Danish", "Jeg vil lære TestNG cool"},
+                {"Dutch", "Ik zal TestNG cool leren"},
+                {"French", "J'apprendrai TestNG cool"},
+                {"Georgian", "მაგრად ვისწავლი TestNG"},
+                {"German", "Ich werde TestNG cool lernen"},
+                {"Irish", "Foghlaimeoidh mé TestNG fionnuar"},
+                {"Italian", "Imparerò il TestNG alla grande"},
+                {"Japanese", "TestNGをかっこよく学びます"},
+                {"Korean", "TestNG 멋지게 배워보겠습니다"},
+                {"Latin", "Discam TestNG frigus"},
+                {"Malay", "Saya akan belajar TestNG sejuk"},
+                {"Polish", "Nauczę się TestNG cool"},
+                {"Portuguese", "Vou aprender TestNG legal"},
+                {"Serbian", "Научићу ТестНГ цоол"},
+                {"Slovak", "Naučím sa TestNG v pohode"},
+                {"Spanish", "Aprenderé TestNG genial"}
+        };
+    }
+
+}
